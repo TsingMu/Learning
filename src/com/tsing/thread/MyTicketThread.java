9169f81f366353301e6b41322437d7b1daf99f80
TsingMu <kingmou@qq.com>
2019-07-29 10:40:21 +0800

    test

--- src/com/tsing/thread/MyTicketThread.java
+++ src/com/tsing/thread/MyTicketThread.java
@@ -2,16 +2,14 @@ package com.tsing.thread;
 
 public class MyTicketThread implements Runnable{
 	private int count = 5;
-	private String name;
-	public  MyTicketThread(String name) {
-		// TODO Auto-generated constructor stub
-		this.name = name;
-	}
-	@Override
+	
+	
 	public void run(){
 		for (int i=0; i<100 ; i++){
 			if(count > 0){
-				System.out.println(name + "sold: " + "ticket = " + count--);
+				count--;
+				System.out.println(Thread.currentThread().getName() + " sold: " + "ticket = " + count);
+				
 			}
 		}
 	}
--- src/com/tsing/thread/SoldTickets.java
+++ src/com/tsing/thread/SoldTickets.java
@@ -4,12 +4,13 @@ public class SoldTickets {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MyTicketThread my1 = new MyTicketThread("my1");
-		MyTicketThread my2 = new MyTicketThread("my2");
-		MyTicketThread my3 = new MyTicketThread("my3");
-		new Thread(my1).start();
-		new Thread(my2).start();
-		new Thread(my3).start();
+		//MyTicketThread my1 = new MyTicketThread();
+		//MyTicketThread my2 = new MyTicketThread("my2");
+		//MyTicketThread my3 = new MyTicketThread("my3");
+		new Thread(new MyTicketThread()).start();
+		new Thread(new MyTicketThread()).start();
+		new Thread(new MyTicketThread()).start();
+		//new Thread(my1).start();
 
 	}
 

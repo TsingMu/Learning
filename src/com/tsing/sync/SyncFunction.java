d97f6536be0424bc5b9d98993af9fa8eed7a5fc7
TsingMu <kingmou@qq.com>
2019-07-31 14:47:51 +0800

    k

---
+++ src/com/tsing/sync/SyncFunction.java
@@ -0,0 +1,27 @@
+package com.tsing.sync;
+
+public class SyncFunction implements Runnable{
+	private int count = 5;
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 100; i++) {
+			this.fun();
+		}
+	}
+	
+	public synchronized void fun(){
+		if(count > 0){
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() +" sold ticket: count= " + count--);
+		}
+	}
+	
+
+}

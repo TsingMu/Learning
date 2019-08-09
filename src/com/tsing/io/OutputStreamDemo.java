7757fcaab07d16320d97f68dcaf05168f2a84210
TsingMu <kingmou@qq.com>
2019-08-09 17:33:38 +0800

    1

--- src/com/tsing/io/OutputStreamDemo.java
+++ src/com/tsing/io/OutputStreamDemo.java
@@ -3,7 +3,7 @@ package com.tsing.io;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
+//Simport java.io.IOException;
 import java.io.OutputStream;
 
 public class OutputStreamDemo {
